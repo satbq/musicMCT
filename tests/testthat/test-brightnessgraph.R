84cd531edbb17006ab6719eab791162a5d515f6f
satbq <120515622+satbq@users.noreply.github.com>
2025-04-14 14:59:20 -0600

    brightnessgraph snapshot with vdiffr

--- tests/testthat/test-brightnessgraph.R
+++ tests/testthat/test-brightnessgraph.R
@@ -3,3 +3,9 @@ test_that("bg_reduction works", {
   expect_equal(bg_reduction(c(0,1,2,7)), goal_matrix)
 })
 
+test_that("brightness graph images consistent", {
+  c_major_19edo <- c(0,3,6,8,11,14,17)
+  cmaj_bg <- function() brightnessgraph(c_major_19edo, edo=19)
+  vdiffr::expect_doppelganger("Brightness Graph for C major in 19edo",
+                               cmaj_bg)
+})
