fb0dcb8e8a6aeb99cf2e0621edfbe1561c8cc9f0
satbq <120515622+satbq@users.noreply.github.com>
2025-04-27 18:31:11 -0600

    Documentation for simplify_scale()

--- R/simplify_scale.R
+++ R/simplify_scale.R
@@ -1,6 +1,36 @@
-#' Simplify a scale
+#' Best ways to regularize a scale
 #' 
-#' simplify_scale description
+#' Given an input scale, identify which adjacent colors represent good
+#' approximations of it, in a sense consistent with "Modal Color Theory,"
+#' pp. 31-32.
+#'
+#' Suppose that you've gathered data on how a particular instrument is tuned.
+#' Two intervals in its scale differ by about 12 cents: does it make sense
+#' to consider those intervals to be essentially the same, up to some
+#' combination of measurement error and the permissiveness of cognitive
+#' categories? `simplify_scale()` helps to answer such a question by considering
+#' whether eliding a precisely measured difference results in a significant
+#' simplifcation of the overall scale structure.
+#'
+#' It accomplishes this by starting from two premises:
+#'  * Any simplification should move to an adjacent color with fewer 
+#'      degrees of freedom.
+#'  * There's a tradeoff between moving farther (i.e. requiring more
+#'      measurement fuzziness) and achieving greater regularity.
+#' Therefore it starts by projecting the input scale onto all neighboring
+#' flats with fewer degrees of freedom. Some projections can be rejected 
+#' immediately because the closest point on the flat isn't actually an 
+#' adjacent color. The non-rejected projections can therefore be ranked by
+#' calculating the "cost" of each additional regularity: for every `1` or `-1`
+#' in the sign vector that is converted to a `0`, how far does one have to
+#' move in voice leading space?
+#'
+#' To answer this question, `simplify_signvector` needs access to data about
+#' the hyperplane arrangement in question. For the basic "Modal Color Theory"
+#' arrangements, this is the data in `representative_scales.rds`, 
+#' `representative_signvectors.rds`, and `color_adjacencies.rds`. The function
+#' assumes that, if you don't specify other data, you have those three files
+#' loaded into your workspace. It can't function without them.
 #'
 #' @inheritParams tnprime
 #' @inheritParams signvector
@@ -19,12 +49,52 @@
 #' @param ... Other arguments to be passed from `best_simplification()`
 #'   to `simplify_scale()`.
 #'
-#' @returns A complicated matrix
+#' @returns A matrix with `n+6` rows, where `n` is the number of notes in the
+#'   scale. Each column represents a scale which is a potential simplification
+#'   of the input `set`, together with details about that simplified scale.
+#'   The first `n` entries of the column represent the pitches of the scale
+#'   itself:
 #'
-#' @examples
-#' \dontrun{
-#'   simplify_scale(c(0,5))
-#' }
+#'   * The `n+1`th row indicates the color number of the simplification.
+#'   * The `n+2`th row shows how many degrees of freedom the simplification has
+#'   (always between `0` and `d-1` where `d` is `set`'s degree of freedom).
+#'   * The `n+3`th row calculates the voice-leading distance from `set` to the
+#'   simplified scale (according to the chosen `method`, for which Euclidean
+#'   distance is the default because it corresponds to the assumption that
+#'   orthogonal projection finds the closest point on a neighboring flat).
+#'   *  The `n+4`th row counts how many more hyperplanes the simplified scale
+#'   lies on compared to `set`. 
+#'   * The `n+5`th row is a quotient of the previous
+#'   two rows (distance divided by number of new regularities). 
+#'   * The `n+6`th row calculates a final "score" which is used to order the
+#'   columns from best (first) to worst (last) simplifications. This score
+#'   is the inverse of the previous row divided by the total number of 
+#'   hyperplanes in the arrangement. (Without this normalization, scores
+#'   for higher cardinalities quickly become much larger than scores for
+#'   low cardinalities.)
+#'  
+#'   If `display_digits` is a value other than `NULL`, the function prints
+#'   to console a suitably rounded representation of the data, while
+#'   invisibly returning the unrounded information.
+#'
+#'   `best_simplification()` returns simply a numeric vector with the scale
+#'    judged optimal by `simplify_scale()` (i.e. the first `n` entries of
+#'    its first column, without all the other information).
+#'
+#' @examplesIf exists("representative_scales") && exists("representative_signvectors") && exists("color_adjacencies")
+#' # For this example to run, you need the necessary data files loaded.
+#' # Let's see what happens if we try to simplify the 5-limit just diatonic:
+#' 
+#' simplify_scale(j(dia))
+#' 
+#' # So the best option is color number 942695, which is the "well-formed"
+#' # structure of the familiar diatonic scale. The particular saturation of
+#' # that meantone structure is very close to 1/5-comma meantone:
+#' 
+#' simplified_jdia <- best_simplification(j(dia))
+#' fifth_comma_dia <- sim(sort((meantone_fifth(1/5)*(0:6))%%12))[,5]
+#' vl_dist(simplified_jdia, fifth_comma_dia)
+#' 
 #' @export
 simplify_scale <- function(set,
                            start_zero=TRUE, 
